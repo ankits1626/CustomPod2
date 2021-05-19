139b595cd4a6a7938ec22ee93d29a3870bec8f2e
Ankit Sachan <ankit@rewardz.sg>
2021-05-19 13:23:14 +0530

    Initial project setup

---
+++ CustomPod2/CommonLogger.swift
@@ -0,0 +1,17 @@
+//
+//  CommonLogger.swift
+//  CustomPod2
+//
+//  Created by Ankit Sachan on 19/05/21.
+//
+
+import Foundation
+public final class CommonLogger {
+
+    public init(){}
+    
+    public func log(error: String){
+        print("<<<<<<<<<< log:------ \(error)")
+    }
+    
+}
---
+++ CustomPod2/CustomPod2.h
@@ -0,0 +1,18 @@
+//
+//  CustomPod2.h
+//  CustomPod2
+//
+//  Created by Ankit Sachan on 19/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomPod2.
+FOUNDATION_EXPORT double CustomPod2VersionNumber;
+
+//! Project version string for CustomPod2.
+FOUNDATION_EXPORT const unsigned char CustomPod2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomPod2/PublicHeader.h>
+
+
